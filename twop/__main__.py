c49e15c1a94739fb9125080b979cc2c235e049d1
Tim Churchard <tim.churchard@gmail.com>
2020-08-19 17:16:55 +0100

    Initial script

---
+++ twop/__main__.py
@@ -0,0 +1,74 @@
+# Copyright Tim Churchard 2020
+
+from .const import ERROR, OK
+from .seed import make_salt, make_seed
+from .wallet import make_address
+
+import click
+
+
+@click.group()
+def main():
+    pass
+
+
+
+@click.command()
+@click.option('--password', help='Password for seed')
+def seed(password: str):
+    password_bytes = password.encode('utf8')
+
+    try:
+        salt = make_salt(password_bytes)
+    except ValueError as exc:
+        print(exc)
+        return ERROR
+
+    try:
+        seed = make_seed(password_bytes, salt)
+    except ValueError as exc:
+        print(exc)
+        return ERROR
+
+    print(f'Made seed. Hex = {seed.hex}\n{seed.mnemonic}')
+    return OK
+
+
+@click.command()
+@click.option('--password', help='Password for seed')
+@click.option('--second', help='Password for wallet')
+@click.option('--num', default=0, help='Address number')
+@click.option('--script', default='p2sh', help='Script (p2pkh, p2wpkh, p2wpkh-p2sh)')
+@click.option('--path', default='wallet.db', help='Path to wallet file')
+def address(password, second, num, script, path):
+    password_bytes = password.encode('utf8')
+    second_bytes = second.encode('utf8')
+
+    try:
+        salt = make_salt(password_bytes)
+    except ValueError as exc:
+        print(exc)
+        return ERROR
+
+    try:
+        seed = make_seed(password_bytes, salt)
+    except ValueError as exc:
+        print(exc)
+        return ERROR
+    # print(f'Made seed. Hex = {seed.hex}\n{seed.mnemonic}\n')
+
+    addr = make_address(seed.mnemonic, second, num, script, path)
+
+    print(f'Made address: {addr.num} = {addr.address}')
+    # print(f'WIF: {addr.wif}')
+
+    return OK
+
+
+main.add_command(seed)
+main.add_command(address)
+
+
+if __name__ == '__main__':
+    from sys import exit
+    exit(main())
---
+++ twop/seed.py
@@ -0,0 +1,46 @@
+# Copyright Tim Churchard 2020
+
+from collections import namedtuple
+from hashlib import sha256, pbkdf2_hmac
+
+from mnemonic import Mnemonic
+
+from .const import DEF_ITS_SALT, DEF_ITS_PBKDF2, MIN_LEN_PASSWORD, MIN_ITS_PBKDF2, MIN_ITS_SALT
+
+
+def make_salt(password: bytes, iterations: int = DEF_ITS_SALT) -> bytes:
+    """make_salt: Takes password bytes and hashes iterations times to produce a salt
+
+    sha256(password) -> sha256(result) -> sha256(result) until iterations
+    """
+    if len(password) < MIN_LEN_PASSWORD:
+        raise ValueError(f'Password too short {len(password)} < {MIN_LEN_PASSWORD}')
+    if iterations < MIN_ITS_SALT:
+        raise ValueError(f'Salt iterations too low {iterations} < {MIN_ITS_SALT}')
+
+    result = password
+    for _ in range(iterations):
+        md = sha256()
+        md.update(result)
+        result = md.digest()
+    return result
+
+
+SeedResult = namedtuple('SeedResult', 'bytes hex mnemonic')
+
+
+def make_seed(password: bytes, salt: bytes, iterations: int = MIN_ITS_PBKDF2, language: str = 'english') -> SeedResult:
+    """make_seed: Takes password and salt and returns namedtuple
+    """
+    if len(password) < MIN_LEN_PASSWORD:
+        raise ValueError(f'Password too short {len(password)} < {MIN_LEN_PASSWORD}')
+    if iterations < MIN_ITS_PBKDF2:
+        raise ValueError(f'Seed iterations too low {iterations} < {MIN_ITS_PBKDF2}')
+    # todo: Make this configurable?
+    if len(salt) != 32:
+        raise ValueError(f'Salt must be 32-bytes')
+
+    dk = pbkdf2_hmac('sha256', password, salt, iterations)
+    seed = Mnemonic(language).to_mnemonic(dk)
+
+    return SeedResult(bytes=dk, hex=dk.hex(), mnemonic=seed)
