d11dc3c76b244ea689cca1eb9d27512a78e43e4a
Timothy Churchard <timchurchard@users.noreply.github.com>
2022-05-19 16:58:43 +0100

    Add python cryptotools mode
    Note! Another incompatible change! Force electrum to make bip39 addrs!

--- twop/wallet.py
+++ twop/wallet.py
@@ -14,6 +14,7 @@ try:
 except ImportError:
     pass
 
+from cryptotools.BTC import Xprv
 from mnemonic import Mnemonic
 
 from .const import MIN_LEN_PASSWORD
@@ -22,7 +23,24 @@ from .const import MIN_LEN_PASSWORD
 AddressResult = namedtuple('AddressResult', 'address wif num')
 
 
-def make_address(mnemonic: str, second: str, num: int = 0, script: str = 'p2wpkh', path: str = 'wallet.db', remove: bool = True):
+def make_address_py(mnemonic: str, second: str, num: int = 0, script: str = 'p2wpkh', hardened: bool = False):
+    script = script.upper()
+    m = Xprv.from_mnemonic(mnemonic, passphrase=second, addresstype=script)
+
+    if hardened:
+        num = float(num)
+
+    if script == 'P2WPKH':
+        addr = (m/84./0./0./0/num)
+    elif script == 'P2WPKH-P2SH':
+        addr = (m/49./0./0./0/num)
+    elif script == 'P2PKH':
+        addr = (m/44./0./0./0/num)
+
+    return AddressResult(address=addr.address(script), wif=f'{script}:{addr.key.wif(compressed=True)}', num=num)
+
+
+def make_address_electrum(mnemonic: str, second: str, num: int = 0, script: str = 'p2wpkh', path: str = 'wallet.db', remove: bool = True):
     if electrum is None:
         raise ImportError('Unable to import electrum.  Follow README instructions.')
 
@@ -32,21 +50,28 @@ def make_address(mnemonic: str, second: str, num: int = 0, script: str = 'p2wpkh
     if script not in script_types:
         raise ValueError(f'Script {script} must be noe of {script_types}')
 
-    # TODO: Right now we have to delete the wallet path, if it exists
-    # - Does not seem to be encrypted? But all the password related funcs raise invalid padding ??
     if os.path.exists(path):
         os.unlink(path)
 
-    language = 'english'  # todo
+    language = 'english'
     seed = Mnemonic(language).to_seed(mnemonic).hex()
 
-    storage = WalletStorage(path)
-    db = WalletDB('', manual_upgrades=False)
-    ks = keystore.from_bip39_seed(mnemonic, second, "m/84'/0'/0'/0")
+    derivation = None
+    if script == 'p2wpkh':
+        derivation = "m/84'/0'/0'"
+    elif script == 'p2wpkh-p2sh':
+        derivation = "m/49'/0'/0'"
+    elif script == 'p2pkh':
+        script = 'standard'
+        derivation = "m/44'/0'/0'"
 
+    ks = keystore.from_bip39_seed(mnemonic, second, derivation, xtype=script)
+
+    db = WalletDB('', manual_upgrades=False)
     db.put('keystore', ks.dump())
     db.put('wallet_type', 'standard')
 
+    storage = WalletStorage(path)
     wallet = Wallet(db, storage, config=SimpleConfig())
     if not storage.file_exists():
         wallet.update_password(old_pw=None, new_pw=second, encrypt_storage=True)
